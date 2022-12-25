5fc8ffc646ccafe5097f83240b29467f5162ff9e
nichiro <ichiro@nnip.org>
2022-12-25 08:51:18 +0000

    Initial Commit

---
+++ HuaweiRtMonitor2/HuaweiRtMonitor2App.swift
@@ -0,0 +1,17 @@
+//
+//  HuaweiRtMonitor2App.swift
+//  HuaweiRtMonitor2
+//
+//  Created by 中橋 一朗 on 2022/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HuaweiRtMonitor2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
